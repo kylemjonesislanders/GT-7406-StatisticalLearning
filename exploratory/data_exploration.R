0f29ce3f5945702cf8be24741cb67ab633eedd4d
Kyle Jones <51127919+kylemjonesislanders@users.noreply.github.com>
2021-11-13 12:33:37 -0700

    updating repository structure

---
+++ exploratory/data_exploration.R
@@ -0,0 +1,14 @@
+# Libraries
+library("ggplot2")
+library("rpart")
+library("randomForest")
+library("caret")
+library("gbm")
+library("glmnet")
+
+# Set working directory
+setwd("/Users/Kyle Jones/OneDrive/Desktop/GT-7406-StatisticalLearning")
+
+# Read in Data
+dat <- read.table(file="./data_files/train.csv", 
+                  sep = ",", header=TRUE)
